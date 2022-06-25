4181e5b8cd70760fb608be483f56f1d7337f6f47
Jeff Shaw <jxjshaw@gmail.com>
2022-06-25 15:16:27 -0400

    initial commit

---
+++ Sketches/triangle-sketch.ino
@@ -0,0 +1,70 @@
+#include <TFT_HX8357_Due.h> // Hardware-specific library
+
+TFT_HX8357_Due tft = TFT_HX8357_Due();       // Invoke custom library
+
+#define DEG2RAD 0.0174532925
+
+void setup(void) {
+  tft.begin();
+
+  tft.setRotation(1);
+
+  tft.fillScreen(HX8357_BLACK);
+
+}
+
+
+void loop() {
+
+  int w = 8;
+  int rx = 160;
+  int ry = 100;
+  
+  for (int n = 0; n < 5; n++) {
+    fillArc(160, 100, 300, 40, rx-n*w, ry-n*w, w, 31-n*6);
+  }
+
+  while(1);
+}
+
+// #########################################################################
+// Draw an arc with a defined thickness
+// #########################################################################
+
+// x,y == coords of centre of arc
+// start_angle = 0 - 359
+// seg_count = number of 3 degree segments to draw (120 => 360 degree arc)
+// rx = x axis radius
+// yx = y axis radius
+// w  = width (thickness) of arc in pixels
+// colour = 16 bit colour value
+// Note if rx and ry are the same an arc of a circle is drawn
+
+int fillArc(int x, int y, int start_angle, int seg_count, int rx, int ry, int w, unsigned int colour)
+{
+
+  byte seg = 3; // Segments are 3 degrees wide = 120 segments for 360 degrees
+  byte inc = 3; // Draw segments every 3 degrees, increase to 6 for segmented ring
+
+  // Draw colour blocks every inc degrees
+  for (int i = start_angle; i < start_angle + seg * seg_count; i += inc) {
+    // Calculate pair of coordinates for segment start
+    float sx = cos((i - 90) * DEG2RAD);
+    float sy = sin((i - 90) * DEG2RAD);
+    uint16_t x0 = sx * (rx - w) + x;
+    uint16_t y0 = sy * (ry - w) + y;
+    uint16_t x1 = sx * rx + x;
+    uint16_t y1 = sy * ry + y;
+
+    // Calculate pair of coordinates for segment end
+    float sx2 = cos((i + seg - 90) * DEG2RAD);
+    float sy2 = sin((i + seg - 90) * DEG2RAD);
+    int x2 = sx2 * (rx - w) + x;
+    int y2 = sy2 * (ry - w) + y;
+    int x3 = sx2 * rx + x;
+    int y3 = sy2 * ry + y;
+
+    tft.fillTriangle(x0, y0, x1, y1, x2, y2, colour);
+    tft.fillTriangle(x1, y1, x2, y2, x3, y3, colour);
+  }
+}
