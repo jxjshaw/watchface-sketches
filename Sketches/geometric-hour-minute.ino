5665a96699b3ae04567681298097713d90266c48
Jeff Shaw <jxjshaw@gmail.com>
2022-06-25 20:55:03 -0400

    new face and update rotation

---
+++ Sketches/geometric-hour-minute.ino
@@ -0,0 +1,212 @@
+// Demo using arcFill to draw ellipses and a segmented elipse
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+#define DEG2RAD 0.0174532925
+
+// #define LOOP_DELAY 10 // Loop delay to slow things down
+
+byte inc = 0;
+unsigned int col = 0;
+
+byte red = 31; // Red is the top 5 bits of a 16 bit colour value
+byte green = 0;// Green is the middle 6 bits
+byte blue = 0; // Blue is the bottom 5 bits
+byte state = 0;
+
+uint32_t targetTime = 0;                    // for next 1 second timeout
+static uint8_t conv2d(const char* p); // Forward declaration needed for IDE 1.6.x
+uint8_t hh = conv2d(__TIME__), mm = conv2d(__TIME__ + 3), ss = conv2d(__TIME__ + 6); // Get H, M, S from compile time
+byte omm = 99, oss = 99;
+unsigned int colour = 0;
+
+
+
+void setup(void) {
+  Serial.begin(115200);
+  tft.begin();
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLACK);
+  targetTime = millis() + 1000;
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+}
+
+
+void loop() {
+
+    if (targetTime < millis()) {
+    // Set next update for 1 second later
+    targetTime = millis() + 1000;
+
+    // Adjust the time values by adding 1 second
+    ss++;              // Advance second
+    if (ss == 60) {    // Check for roll-over
+      ss = 0;          // Reset seconds to zero
+      omm = mm;        // Save last minute time for display update
+      mm++;            // Advance minute
+      if (mm > 59) {   // Check for roll-over
+        mm = 0;
+        hh++;          // Advance hour
+        if (hh > 23) { // Check for 24hr roll-over (could roll-over on 13)
+          hh = 0;      // 0 for 24 hour clock, set to 1 for 12 hour clock
+        }
+      }
+    }
+
+
+
+  fillArc(120, 120, 0, mm + 2, 120, 120, 90, 0x7BEF);
+
+  // if two digits then less x
+  tft.drawNumber(hh, 106, 108, 4);  
+
+  //fillArc(120, 120, 0, 60, 8, 8, 2, TFT_WHITE);
+ }
+
+
+  inc++;
+  col += 1;
+  if (col > 191) col = 0;
+  if (inc > 59) inc = 0;
+
+  // this might be the wrong value
+  if (mm > 58 ) {  
+      fillArc(120, 120, 0, 60, 120, 120, 103, TFT_BLACK);
+    }
+  
+
+  // delay(LOOP_DELAY);
+}
+
+
+// #########################################################################
+// Draw a circular or elliptical arc with a defined thickness
+// #########################################################################
+
+// x,y == coords of centre of arc
+// start_angle = 0 - 359
+// seg_count = number of 6 degree segments to draw (60 => 360 degree arc)
+// rx = x axis outer radius
+// ry = y axis outer radius
+// w  = width (thickness) of arc in pixels
+// colour = 16 bit colour value
+// Note if rx and ry are the same then an arc of a circle is drawn
+
+void fillArc(int x, int y, int start_angle, int seg_count, int rx, int ry, int w, unsigned int colour)
+{
+
+  byte seg = 6; // Segments are 3 degrees wide = 120 segments for 360 degrees
+  byte inc = 6; // Draw segments every 3 degrees, increase to 6 for segmented ring
+
+  // Calculate first pair of coordinates for segment start
+  float sx = cos((start_angle - 90) * DEG2RAD);
+  float sy = sin((start_angle - 90) * DEG2RAD);
+  uint16_t x0 = sx * (rx - w) + x;
+  uint16_t y0 = sy * (ry - w) + y;
+  uint16_t x1 = sx * rx + x;
+  uint16_t y1 = sy * ry + y;
+
+  // Draw colour blocks every inc degrees
+  for (int i = start_angle; i < start_angle + seg * seg_count; i += inc) {
+
+    // Calculate pair of coordinates for segment end
+    float sx2 = cos((i + seg - 90) * DEG2RAD);
+    float sy2 = sin((i + seg - 90) * DEG2RAD);
+    int x2 = sx2 * (rx - w) + x;
+    int y2 = sy2 * (ry - w) + y;
+    int x3 = sx2 * rx + x;
+    int y3 = sy2 * ry + y;
+
+    tft.fillTriangle(x0, y0, x1, y1, x2, y2, colour);
+    tft.fillTriangle(x1, y1, x2, y2, x3, y3, colour);
+
+    // Copy segment end to sgement start for next segment
+    x0 = x2;
+    y0 = y2;
+    x1 = x3;
+    y1 = y3;
+  }
+}
+
+// #########################################################################
+// Return the 16 bit colour with brightness 0-100%
+// #########################################################################
+unsigned int brightness(unsigned int colour, int brightness)
+{
+  byte red   = colour >> 11;
+  byte green = (colour & 0x7E0) >> 5;
+  byte blue  = colour & 0x1F;
+
+  blue =  (blue * brightness) / 100;
+  green = (green * brightness) / 100;
+  red =   (red * brightness) / 100;
+
+  return (red << 11) + (green << 5) + blue;
+}
+
+// #########################################################################
+// Return a 16 bit rainbow colour
+// #########################################################################
+unsigned int rainbow(byte value)
+{
+  // Value is expected to be in range 0-127
+  // The value is converted to a spectrum colour from 0 = blue through to 127 = red
+
+  switch (state) {
+    case 0:
+      green ++;
+      if (green == 64) {
+        green = 63;
+        state = 1;
+      }
+      break;
+    case 1:
+      red--;
+      if (red == 255) {
+        red = 0;
+        state = 2;
+      }
+      break;
+    case 2:
+      blue ++;
+      if (blue == 32) {
+        blue = 31;
+        state = 3;
+      }
+      break;
+    case 3:
+      green --;
+      if (green == 255) {
+        green = 0;
+        state = 4;
+      }
+      break;
+    case 4:
+      red ++;
+      if (red == 32) {
+        red = 31;
+        state = 5;
+      }
+      break;
+    case 5:
+      blue --;
+      if (blue == 255) {
+        blue = 0;
+        state = 0;
+      }
+      break;
+  }
+  return red << 11 | green << 5 | blue;
+}
+
+
+// Function to extract numbers from compile time string
+static uint8_t conv2d(const char* p) {
+  uint8_t v = 0;
+  if ('0' <= *p && *p <= '9')
+    v = *p - '0';
+  return 10 * v + *++p - '0';
+}
